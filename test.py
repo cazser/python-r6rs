1c2ea021959e03132724462bf780895958d4d88e
cazser <2264712072@qq.com>
2023-09-15 11:59:18 +0800

    pitch

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 from tokenReader import tokenReader
 from value import Value
 
-class TestTokn(unittest.TestCase):
+class TestToken(unittest.TestCase):
     def testBoolean(self):
         token = tokenReader("#t")
         self.assertEqual(token.getRest(), "#t")
@@ -13,7 +13,8 @@ class TestTokn(unittest.TestCase):
         #print(token.getNext())
         self.assertEqual(token.getNext(), "(+ a 1)")
         self.assertEqual(token.getNext(), "11")
-    
+
+class TestValue(unittest.TestCase):    
     def testValue(self):
         token = tokenReader("#t #f")
         value1 = Value(token.getNext());
