72dc1c51ce97b41908bc8c0b0de86223fb806940
cazser <2264712072@qq.com>
2023-09-16 14:38:42 +0800

    break into pieces

--- Expression.py
+++ Expression.py
@@ -1,4 +1,4 @@
-
+from tokenReader import tokenReader;
 
 
 class Expression:
@@ -9,9 +9,21 @@ class Expression:
         if token[0]=='(':
             self.type ="Compose"
             self.code = token[1:-1]
+            tokens = tokenReader(self.code);
+            while not tokens.isEnd():
+                t = Expression(tokens.getNext());
+                if t.getType() =="Single":
+                    self.list.append(t.getCode())
+                else:
+                    self.list.append(t.getList());
+            
         else:
             self.type ="Single"
             self.code = token
+            self.list.append(token);
+
+    def getList(self):
+        return self.list
 
     def getType(self):
         return self.type
@@ -24,4 +36,4 @@ class Expression:
 
 
     def __str__(self) -> str:
-        pass
+        return str(self.list)
--- test.py
+++ test.py
@@ -23,10 +23,13 @@ class TestExpression(unittest.TestCase):
         self.assertEqual(exp1.getCode(), "(+ a 1)")
         self.assertEqual(exp1.getType(), "Compose")
         self.assertEqual(exp1.getInside(), "+ a 1")
+        self.assertEqual(str(exp1), "['+', 'a', '1']")
         exp1 = Expression(token.getNext())
         self.assertEqual(exp1.getCode(), "11")
         self.assertEqual(exp1.getType(), "Single")
-
+        token1 = tokenReader("(+ 2 3 (+ 3 4 (- 9 7) (+ 100 100) ) )")
+        exp2 = Expression(token1.getNext());
+        self.assertEqual("", str(exp2))
        
 
 
