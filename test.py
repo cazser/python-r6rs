f67d112b7e334b3c68483f8d1c319645ccededf3
cazser <2264712072@qq.com>
2023-09-16 13:54:21 +0800

    value part rewrite

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import unittest
 from tokenReader import tokenReader
 from value import Value
 from Eval import evaluate1
-"""
 class TestToken(unittest.TestCase):
     def testBoolean(self):
         token = tokenReader("#t")
@@ -15,67 +14,6 @@ class TestToken(unittest.TestCase):
         self.assertEqual(token.getNext(), "(+ a 1)")
         self.assertEqual(token.getNext(), "11")
 
-class TestValue(unittest.TestCase):    
-    def testValue(self):
-        token = tokenReader("#t #f")
-        value1 = Value(token.getNext());
-        self.assertEqual(value1.type, "Boolean");
-        self.assertEqual(value1.value, True);
-        value2 = Value(token.getNext());
-        self.assertEqual(value2.type,"Boolean");
-        self.assertEqual(value2.value, False);
-        self.assertEqual(token.isEnd(), True);
-        token1 = tokenReader("123");
-        value3 = Value(token1.getNext());
-        self.assertEqual(value3.type,"Number");
-        self.assertEqual(value3.value, 123);
-        token2 = tokenReader("(+ 1 2)")
-        value4 = Value(token2.getNext());
-        self.assertEqual(value4.type, "Expression")
-        self.assertEqual(value4.inside, "+ 1 2")
-
- 
-class TestEval(unittest.TestCase):
-    def testEval(self):
-        token = tokenReader("(+ 1 2 3)")
-        value = Value(token.getNext())
-        eval = evaluate1();
-        self.assertEqual(eval.eval(value).value, 6)
-        token1 = tokenReader("(- 6 1 3)")
-        value1 = Value(token1.getNext())
-        eval1 = evaluate1();
-        self.assertEqual(eval1.eval(value1).value, 2)
-        token2 = tokenReader("(* 6 2 3)")
-        value2 = Value(token2.getNext())
-        eval2 = evaluate1();
-        self.assertEqual(eval2.eval(value2).value, 36)
-        token3 = tokenReader("(/ 24 2 3)")
-        t = token3.getNext();
-        #print(t);
-        value3 = Value( t );
-        self.assertEqual(eval1.eval(value3).value, 4);
-        token4 = tokenReader("(* (+ 1 2) (/ 24 2 3))")
-        t = token4.getNext();
-        #complex aritemetic
-        value4 = Value( t );
-        self.assertEqual(eval1.eval(value4).value, 12);
-        
-        token5 = tokenReader("(* (- 7 4) (+ 2 1))")
-        t = token5.getNext();
-        #complex aritemetic
-        value5 = Value( t );
-        self.assertEqual(eval1.eval(value5).value, 9);
-"""        
-
-
-class TestIdentifier(unittest.TestCase):
-    def testIdentifier(self):
-        token = tokenReader("(define a 5) a")
-        value = Value(token.getNext());
-        eval = evaluate1()
-        eval.eval(value)
-        value1= Value(token.getNext())
-        print(eval.eval(value1));
 
 
 
--- value.py
+++ value.py
@@ -4,45 +4,8 @@
 class Value:
 
     def __init__(self, token):
-        self.value = {}
-        #print(token)
-        if token=='':
-            self.value="Null"
-            self.type="Null"
-       
-        elif token[0]=='#':
-            self.type="Boolean"
-            if token[1]=='t':
-                self.value = True
-            elif token[1]=='f':
-                self.value = False
-        elif token[0] in '0123456789':
-            self.type = "Number"
-            self.value = int(token)
-        elif token[0]=='(':
-            self.type="Expression"
-            self.inside = token[1:-1]
-        elif token[0]=='+':
-            self.type="Add"
-            self.name="Add"
-        elif token[0]=='-':
-            self.type="Sub"
-            self.name="Sub"
-        elif token[0]=='*':
-            self.type="Mult"
-            self.name="Mult"
-        elif token[0]=='/':
-            self.type="Div"
-            self.name="Div"
-        elif token=='define':
-            self.value="define"
-            self.type="define"
-            self.name='define'
-        else:
-            
-            self.type="Idenetifier"
-            self.name = token
-            print(self)
+        pass
+
 
     def __str__(self) -> str:
         if self.type=="Expression":
