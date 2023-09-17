122c0354935bab25db58c6363a9bd14d7ebfbd96
cazser <2264712072@qq.com>
2023-09-17 16:15:17 +0800

    repl can work

--- repl.py
+++ repl.py
@@ -1,14 +1,28 @@
 from tokenReader import tokenReader
 from value import Value
 from Eval import evaluate1
+from Expression import Expression
 def repl():
-    prompt="->>"
-    print(prompt)
-    line= input();
+    prompt="<"
+    prompt1=">"
+    index = 1
+    line=""
     while not line=="#quit":
+        print(prompt+ str(index) + prompt1, end="")
         line=input();
+        token_Reader = tokenReader(line)
         eval = evaluate1()
-        print(eval.eval(Value(tokenReader(line))))
+        while not token_Reader.isEnd():
+            t= token_Reader.getNext()
+            #print(t)
+            exp = Expression(t)            
+            value = Value(exp.getList())
+            #for it in value.getList():
+            #    print(it)
+            result= eval.eval(value.getList())
+            if not result== None:
+                print(result.value)
+            index= index+1
     print("thanks")
 
  
