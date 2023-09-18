45f17ce606c67f2bd9bd1e4acfbb7581ab5945e0
cazser <2264712072@qq.com>
2023-09-18 11:08:35 +0800

    更新测试

--- Eval.py
+++ Eval.py
@@ -7,7 +7,11 @@ from value import Object
 
 
 class evaluate1:
-    pass    
+    def __init__(self, stack=[]) -> None:
+        self.__stack__ = stack
+
+    def getStack(self):
+        return self.__stack__    
 
 if __name__=='__main__':
     pass
