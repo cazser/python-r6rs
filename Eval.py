7987df5107cddc7288ce1235b34b42b03e043897
cazser <2264712072@qq.com>
2023-09-18 14:07:50 +0800

    begin to implement

--- Eval.py
+++ Eval.py
@@ -9,6 +9,19 @@ from value import Object
 class evaluate1:
     def __init__(self, stack=[]) -> None:
         self.__stack__ = stack
+    
+    def eval(self, objList: list[Object]):
+        for it in objList:
+            print(it)
+        if len(objList)>1:
+            first = objList[0]
+            rest = objList[1:]
+            if first["type"] =="keyword":
+                if first["name"] == "define":
+                    self.defineVar(rest[0], rest[1])
+            
+    def defineVar(self, item1, item2):
+        pass
 
     def getStack(self):
         return self.__stack__    
--- test.py
+++ test.py
@@ -7,7 +7,10 @@ from Expression import Expression
 
 class TestBasicValue(unittest.TestCase):
     def testNumber(self):
-        pass
+        token_reader = tokenReader("(define a 5)")
+        value = Value(Expression(token_reader.getNext()).getList())
+        eval = evaluate1()
+        eval.eval(value.getList())
 
 if __name__=="__main__":
     unittest.main()
